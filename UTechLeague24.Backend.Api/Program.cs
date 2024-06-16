4382dd9c1608321ebff105b93e3b4372922f6002
Izzat Alsharif <work@izzatalsharif.com>
2024-06-16 23:56:29 +0300

    Add MemoryCache option

--- UTechLeague24.Backend.Api/Program.cs
+++ UTechLeague24.Backend.Api/Program.cs
@@ -6,6 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDomainServices(builder.Configuration);
 builder.Services.AddAuthenticationServices(builder.Configuration);
 builder.Services.AddControllers();
+
+// Other services and configurations.
+builder.Services.AddOptions();
+builder.Services.AddMemoryCache();
 builder.Services.AddProblemDetails();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
